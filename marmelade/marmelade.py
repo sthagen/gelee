411ebc7b20a6d54f956245f694dd2b410e65005f
Stefan Hagen <stefan@hagen.link>
2021-04-13 21:59:44 +0200

    Update marmelade.py

--- marmelade/marmelade.py
+++ marmelade/marmelade.py
@@ -1,1 +1,54 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=c-extension-no-member,expression-not-assigned,line-too-long,logging-fstring-interpolation
+"""Do the lint."""
+import json
+import logging
+import os
+import pathlib
+import sys
+import typing
+
+import jsonschema  # type: ignore
+from lxml import etree  # type: ignore
+
+
+ENCODING = "utf-8"
+
+APP = 'marmelade'
+
+LOG = logging.getLogger()  # Temporary refactoring: module level logger
+LOG_FOLDER = pathlib.Path('logs')
+LOG_FILE = f'{APP}.log'
+LOG_PATH = pathlib.Path(LOG_FOLDER, LOG_FILE) if LOG_FOLDER.is_dir() else pathlib.Path(LOG_FILE)
+LOG_LEVEL = logging.INFO
+
+
+def init_logger(name=None, level=None):
+    """Initialize module level logger"""
+    global LOG  # pylint: disable=global-statement
+
+    log_format = {
+        'format': '%(asctime)s %(levelname)s [%(name)s]: %(message)s',
+        'datefmt': '%Y-%m-%d %H:%M:%S',
+        # 'filename': LOG_PATH,
+        'level': LOG_LEVEL if level is None else level
+    }
+    logging.basicConfig(**log_format)
+    LOG = logging.getLogger(APP if name is None else name)
+
+
+def main(argv=None, embedded=False, debug=None):
+    """Drive the validator.
+    This function acts as the command line interface backend.
+    There is some duplication to support testability.
+    """
+    debug = DEBUG if debug is None else debug is True  # debug is None and DEBUG or debug is True
+    init_logger(level=logging.DEBUG if debug else None)
+    argv = argv if argv else sys.argv[1:]
+    if not argv:
+        print("Usage: marmelade paths-to-files")
+        return 0
+    num_args = len(argv)
+    LOG.debug(f"guarded dispatch {argv=}, {num_args=}")
+    return 0
 
