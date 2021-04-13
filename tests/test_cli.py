1537a896d91d73891653fd3d5722b26523b57b12
Stefan Hagen <stefan@hagen.link>
2021-04-13 21:52:35 +0200

    Update test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,1 +1,14 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long,missing-docstring,reimported,unused-import,unused-variable
+import pathlib
 
+import pytest  # type: ignore
+
+import marmelade.cli as cli
+
+
+def test_main_ok_no_args(capsys):
+    assert cli.main([], debug=False) == 0
+    out, err = capsys.readouterr()
+    assert "usage" in out.lower()
+    assert not err
