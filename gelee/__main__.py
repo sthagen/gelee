8226111d4ceba5976bb69a948db8a3ab83d2c30d
Stefan Hagen <stefan@hagen.link>
2021-04-25 21:21:03 +0200

    Transient refactoring of names part five

--- gelee/__main__.py
+++ gelee/__main__.py
@@ -2,7 +2,7 @@
 # pylint: disable=expression-not-assigned,line-too-long,missing-module-docstring
 import sys
 
-from marmelade.cli import main
+from gelee.cli import main
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))  # pragma: no cover
