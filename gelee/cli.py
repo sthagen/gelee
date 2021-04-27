e8997e187e4fe5f1c412387d2a2f6609d5e8bf61
Stefan Hagen <stefan@hagen.link>
2021-04-27 21:52:25 +0200

    Added abort environment var and parameter

--- gelee/cli.py
+++ gelee/cli.py
@@ -11,19 +11,23 @@ import gelee.gelee as lint
 DEBUG_VAR = "GELEE_DEBUG"
 DEBUG = bool(os.getenv(DEBUG_VAR))
 
+ABORT_VAR = "GELEE_ABORT"
+ABORT = bool(os.getenv(ABORT_VAR))
+
 
 # pylint: disable=expression-not-assigned
-def main(argv=None, debug=None):
+def main(argv=None, abort=None, debug=None):
     """Dispatch processing of the job.
     This is the strings only command line interface.
     For python API use interact with lint functions directly.
     """
     argv = sys.argv[1:] if argv is None else argv
     debug = debug if debug else DEBUG
+    abort = abort if abort else ABORT
     for arg in argv:
         if not pathlib.Path(arg).is_file() and not pathlib.Path(arg).is_dir():
             print("ERROR: For now only paths understood.")
             sys.exit(2)
 
-    code, _ = lint.main(argv, debug=debug)
+    code, _ = lint.main(argv, abort=abort, debug=debug)
     return code
