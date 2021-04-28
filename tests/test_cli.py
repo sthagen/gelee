3a89e252720883d5b232330f1866559c6224bbdc
Stefan Hagen <stefan@hagen.link>
2021-04-28 22:14:08 +0200

    Added positive and negative test on single file path

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -14,11 +14,27 @@ def test_main_ok_no_args(capsys):
     assert not err
 
 
+def test_main_ok_single_valid_file_as_arg(capsys):
+    assert cli.main(["tests/fixtures/valid/json/empty_object.json"], debug=False) == 0
+    out, err = capsys.readouterr()
+    assert "usage" not in out.lower()
+    assert "ok" in out.lower()
+    assert not err
+
+
 def test_main_ok_tests_fixtures_valid_as_arg(capsys):
     assert cli.main(["tests/fixtures/valid/"], debug=False) == 0
     out, err = capsys.readouterr()
     assert "usage" not in out.lower()
     assert "ok" in out.lower()
+    assert not err
+
+
+def test_main_nok_single_invalid_file_as_arg(capsys):
+    assert cli.main(["tests/fixtures/invalid/ini/missing_section_header.ini"], debug=False) == 0
+    out, err = capsys.readouterr()
+    assert "usage" not in out.lower()
+    assert "fail" in out.lower()
     assert not err
 
 
