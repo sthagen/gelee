ff8e157093a50f845f840b922cd664e6aaf4aa56
Stefan Hagen <stefan@hagen.link>
2022-12-26 15:24:03 +0100

    Added management tools

--- gelee/__init__.py
+++ gelee/__init__.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
 """Validate configuration files against their format and maybe schema."""
-__version__ = '2022.11.28'
+# [[[fill git_describe()]]]
+__version__ = '2022.11.28+parent.7cef30b3-dirty'
+# [[[end]]] (checksum: 292b24b2ea3ee44d0bfa30684335287e)
+__version_info__ = tuple(
+    e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
+)
